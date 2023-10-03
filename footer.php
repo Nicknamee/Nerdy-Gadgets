4656ff3d529dc140c53b81b5e65fedbe8fb7a4d1
chynob2 <s1202437@student.windesheim.nl>
2023-10-03 14:36:16 +0200

    ik heb de footer toegevoegd!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ footer.php
@@ -0,0 +1,49 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="Nerdy%20gadgets.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+          integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+</head>
+<body>
+<footer>
+    <div class= "footerContainer">
+        <div class="socialIcons">
+            <a href=""><i class="fa-brands fa-facebook "></i></a>
+            <a href=""><i class="fa-brands fa-instagram "></i></a>
+            <a href=""><i class="fa-brands fa-twitter "></i></a>
+            <a href=""><i class="fa-brands fa-google plus "></i></a>
+
+        </div>
+        <div class="footernav">
+            <ul> <li><a href="">about us</a> </li>
+                <li><a href="">contact us</a> </li>
+                <li><a href="">get a job</a> </li>
+                <li><a href="">our team </a> </li>
+                <li><a href="">customer service </a> </li>
+
+            </ul>
+        </div>
+
+
+    </div>
+
+    <div class="footerBottom">
+        <p>Copyright &copy;2022; Designed <span class="designer">Nerdy Gadgets Team 4</span></p>
+
+    </div>
+</footer>
+
+</body>
+</html>
