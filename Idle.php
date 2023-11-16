e156a09dbdd0883a925d50a4afe64c5e7029ba8e
Deankw5 <146432051+Deankw5@users.noreply.github.com>
2023-11-16 11:30:01 +0100

    Add files via upload

---
+++ Idle.php
@@ -0,0 +1,16 @@
+<?php
+<configuration>
+<system.web>
+   <sessionState timeout="10"></sessionState>
+</system.web>
+</configuration>
+
+<forms 
+   timeout="10">
+</forms>
+
+<forms    
+   timeout="10"
+   slidingExpiration="true">
+</forms>
+?>
